1625d535768bb7fc5270c4a4e61b9389f3f87df3
John Rofrano <johnnyroy@johnrofrano.com>
2020-03-17 01:31:56 +0000

    added address methods with tests

--- service/service.py
+++ service/service.py
@@ -20,15 +20,118 @@ from service.models import Account, Address, DataValidationError
 from . import app
 
 ######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
+
+
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+        ),
+        status.HTTP_400_BAD_REQUEST,
+    )
+
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
+        status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            error="Method not Allowed",
+            message=message,
+        ),
+        status.HTTP_405_METHOD_NOT_ALLOWED,
+    )
+
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = str(error)
+    app.logger.warning(message)
+    return (
+        jsonify(
+            status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            error="Unsupported media type",
+            message=message,
+        ),
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    )
+
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = str(error)
+    app.logger.error(message)
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=message,
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
+
+######################################################################
 # GET INDEX
 ######################################################################
 @app.route("/")
 def index():
     """ Root URL response """
-    return "Reminder: return some useful information in json format about the service here", status.HTTP_200_OK
+    return (
+        jsonify(
+            name="Account REST API Service",
+            version="1.0",
+            paths=url_for("list_accounts", _external=True),
+        ),
+        status.HTTP_200_OK,
+    )
 
 ######################################################################
-# RETRIEVE A PET
+# LIST ALL ACCOUNTS
+######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """ Returns all of the Accounts """
+    app.logger.info("Request for Account list")
+    accounts = []
+    name = request.args.get("name")
+    if name:
+        accounts = Account.find_by_name(name)
+    else:
+        accounts = Account.all()
+
+    results = [account.serialize() for account in accounts]
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+
+######################################################################
+# RETRIEVE AN ACCOUNT
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
@@ -38,9 +141,7 @@ def get_accounts(account_id):
     This endpoint will return an Account based on it's id
     """
     app.logger.info("Request for Account with id: %s", account_id)
-    account = Account.find(account_id)
-    if not account:
-        raise NotFound("Account with id '{}' was not found.".format(account_id))
+    account = Account.find_or_404(account_id)
     return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
 
 
@@ -65,21 +166,114 @@ def create_accounts():
     )
 
 ######################################################################
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+
+    This endpoint will update an Account based the body that is posted
+    """
+    app.logger.info("Request to update account with id: %s", account_id)
+    check_content_type("application/json")
+    account = Account.find(account_id)
+    if not account:
+        raise NotFound("Account with id '{}' was not found.".format(account_id))
+    account.deserialize(request.get_json())
+    account.id = account_id
+    account.save()
+    return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# DELETE AN ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+
+    This endpoint will delete an Account based the id specified in the path
+    """
+    app.logger.info("Request to delete account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return make_response("", status.HTTP_204_NO_CONTENT)
+
+
+#---------------------------------------------------------------------
+#                A D D R E S S   M E T H O D S
+#---------------------------------------------------------------------
+
+
+######################################################################
 # ADD AN ADDRESS TO AN ACCOUNT
 ######################################################################
 @app.route('/accounts/<int:account_id>/addresses', methods=['POST'])
-def add_address_to_account(account_id):
-    app.logger.info("Request to add an address to an account")
-    account = Account.find(account_id)
-    if not account:
-        abort(404, "Account not found")
+def create_addresses(account_id):
+    """
+    Create an Address on an Account
 
+    This endpoint will add an address to an account
+    """
+    app.logger.info("Request to add an address to an account")
+    check_content_type("application/json")
+    account = Account.find_or_404(account_id)
     address = Address()
     address.deserialize(request.get_json())
-    account.address.append(address)
+    account.addresses.append(address)
     account.save()
     message = address.serialize()
     return make_response(jsonify(message), status.HTTP_201_CREATED)
+
+######################################################################
+# RETRIEVE AN ADDRESS FROM ACCOUNT
+######################################################################
+@app.route('/accounts/<int:account_id>/addresses/<int:address_id>', methods=['GET'])
+def get_addresses(account_id, address_id):
+    """
+    Get an Address
+
+    This endpoint returns just an address
+    """
+    app.logger.info("Request to get an address with id: %s", address_id)
+    address = Address.find_or_404(address_id)
+    return make_response(jsonify(address.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# UPDATE AN ADDRESS
+######################################################################
+@app.route("/accounts/<int:account_id>/addresses/<int:address_id>", methods=["PUT"])
+def update_addresses(account_id, address_id):
+    """
+    Update an Address
+
+    This endpoint will update an Address based the body that is posted
+    """
+    app.logger.info("Request to update address with id: %s", address_id)
+    check_content_type("application/json")
+    address = Address.find_or_404(address_id)
+    address.deserialize(request.get_json())
+    address.id = address_id
+    address.save()
+    return make_response(jsonify(address.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# DELETE AN ADDRESS
+######################################################################
+@app.route("/accounts/<int:account_id>/addresses/<int:address_id>", methods=["DELETE"])
+def delete_addresses(account_id, address_id):
+    """
+    Delete an Address
+
+    This endpoint will delete an Address based the id specified in the path
+    """
+    app.logger.info("Request to delete account with id: %s", account_id)
+    address = Address.find(address_id)
+    if address:
+        address.delete()
+    return make_response("", status.HTTP_204_NO_CONTENT)
 
 
 
--- tests/test_service.py
+++ tests/test_service.py
@@ -51,13 +51,66 @@ class TestYourResourceServer(TestCase):
         db.drop_all()
 
 ######################################################################
-#  P L A C E   T E S T   C A S E S   H E R E 
+#  H E L P E R   M E T H O D S
+######################################################################
+
+    def _create_accounts(self, count):
+        """ Factory method to create accounts in bulk """
+        accounts = []
+        for _ in range(count):
+            account = AccountFactory()
+            resp = self.app.post(
+                "/accounts", json=account.serialize(), content_type="application/json"
+            )
+            self.assertEqual(
+                resp.status_code, status.HTTP_201_CREATED, "Could not create test Account"
+            )
+            new_account = resp.get_json()
+            account.id = new_account["id"]
+            accounts.append(account)
+        return accounts
+
+######################################################################
+#  A C C O U N T   T E S T   C A S E S
 ######################################################################
 
     def test_index(self):
         """ Test index call """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_get_account_list(self):
+        """ Get a list of Accounts """
+        self._create_accounts(5)
+        resp = self.app.get("/accounts")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_account_by_name(self):
+        """ Get a Account by Name """
+        accounts = self._create_accounts(3)
+        resp = self.app.get("/accounts?name={}".format(accounts[1].name))
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data[0]["name"], accounts[1].name)
+
+    def test_get_account(self):
+        """ Get a single Account """
+        # get the id of an account
+        account = self._create_accounts(1)[0]
+        resp = self.app.get(
+            "/accounts/{}".format(account.id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_get_account_not_found(self):
+        """ Get an Account that is not found """
+        resp = self.app.get("/accounts/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_create_account(self):
         """ Create a new Account """
@@ -90,3 +143,187 @@ class TestYourResourceServer(TestCase):
         self.assertEqual(new_account["email"], account.email, "Email does not match")
         self.assertEqual(new_account["phone_number"], account.phone_number, "Phone does not match")
         self.assertEqual(new_account["date_joined"], str(account.date_joined), "Date Joined does not match")
+
+    def test_update_account(self):
+        """ Update an existing Account """
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.app.post(
+            "/accounts", 
+            json=test_account.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the pet
+        new_account = resp.get_json()
+        new_account["name"] = "Happy-Happy Joy-Joy"
+        resp = self.app.put(
+            "/accounts/{}".format(new_account["id"]),
+            json=new_account,
+            content_type="application/json",
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Happy-Happy Joy-Joy")
+
+    def test_delete_account(self):
+        """ Delete an Account """
+        # get the id of an account
+        account = self._create_accounts(1)[0]
+        resp = self.app.delete(
+            "/accounts/{}".format(account.id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_bad_request(self):
+        """ Send wrong media type """
+        account = AccountFactory()
+        resp = self.app.post(
+            "/accounts", 
+            json={"name": "not enough data"}, 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_unsupported_media_type(self):
+        """ Send wrong media type """
+        account = AccountFactory()
+        resp = self.app.post(
+            "/accounts", 
+            json=account.serialize(), 
+            content_type="test/html"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_method_not_allowed(self):
+        """ Make an illegal method call """
+        resp = self.app.put(
+            "/accounts", 
+            json={"not": "today"}, 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+######################################################################
+#  A D D R E S S   T E S T   C A S E S
+######################################################################
+
+    def test_add_address(self):
+        """ Add an address to an account """
+        account = self._create_accounts(1)[0]
+        address = AddressFactory()
+        resp = self.app.post(
+            "/accounts/{}/addresses".format(account.id), 
+            json=address.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["account_id"], account.id)
+        self.assertEqual(data["name"], address.name)
+        self.assertEqual(data["street"], address.street)
+        self.assertEqual(data["city"], address.city)
+        self.assertEqual(data["state"], address.state)
+        self.assertEqual(data["postalcode"], address.postalcode)
+
+    def test_get_address(self):
+        """ Get an address from an account """
+        # create a known address
+        account = self._create_accounts(1)[0]
+        address = AddressFactory()
+        resp = self.app.post(
+            "/accounts/{}/addresses".format(account.id), 
+            json=address.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        address_id = data["id"]
+
+        # retrieve it back
+        resp = self.app.get(
+            "/accounts/{}/addresses/{}".format(account.id, address_id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["account_id"], account.id)
+        self.assertEqual(data["name"], address.name)
+        self.assertEqual(data["street"], address.street)
+        self.assertEqual(data["city"], address.city)
+        self.assertEqual(data["state"], address.state)
+        self.assertEqual(data["postalcode"], address.postalcode)
+
+    def test_update_address(self):
+        """ Update an address on an account """
+        # create a known address
+        account = self._create_accounts(1)[0]
+        address = AddressFactory()
+        resp = self.app.post(
+            "/accounts/{}/addresses".format(account.id), 
+            json=address.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        data = resp.get_json()
+        logging.debug(data)
+        address_id = data["id"]
+        data["name"] = "XXXX"
+
+        # send the update back
+        resp = self.app.put(
+            "/accounts/{}/addresses/{}".format(account.id, address_id), 
+            json=data, 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        # retrieve it back
+        resp = self.app.get(
+            "/accounts/{}/addresses/{}".format(account.id, address_id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        logging.debug(data)
+        self.assertEqual(data["id"], address_id)
+        self.assertEqual(data["account_id"], account.id)
+        self.assertEqual(data["name"], "XXXX")
+
+    def test_delete_address(self):
+        """ Delete an Address """
+        account = self._create_accounts(1)[0]
+        address = AddressFactory()
+        resp = self.app.post(
+            "/accounts/{}/addresses".format(account.id), 
+            json=address.serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        data = resp.get_json()
+        logging.debug(data)
+        address_id = data["id"]
+
+        # send delete request
+        resp = self.app.delete(
+            "/accounts/{}/addresses/{}".format(account.id, address_id),
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+        # retrieve it back and make sure address is not there
+        resp = self.app.get(
+            "/accounts/{}/addresses/{}".format(account.id, address_id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
