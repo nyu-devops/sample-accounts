4149f03aeb17d832f61cef116ac28dfb4fca9976
John Rofrano <johnnyroy@johnrofrano.com>
2022-03-11 14:51:40 -0500

    updated postresql prefix

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import logging
 
 
 # Get configuration from environment
-DATABASE_URI = os.getenv("DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres")
+DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres")
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
