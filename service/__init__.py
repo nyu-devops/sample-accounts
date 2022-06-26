237310f7c2fd4aa5eef8f25052ec1b33a4f72723
John Rofrano <johnnyroy@johnrofrano.com>
2022-06-26 23:14:20 +0000

    Moved config module into service package

--- service/__init__.py
+++ service/__init__.py
@@ -20,11 +20,12 @@ and SQL database
 """
 import sys
 from flask import Flask
+from service import config
 from service.utils import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
-app.config.from_object("config")
+app.config.from_object(config)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
